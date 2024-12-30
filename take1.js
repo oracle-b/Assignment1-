90e3e82e36f97567ae67da20c7d3886519575efb
oracle-b <m.faizrosli11@gmail.com>
2024-12-30 19:03:15 +0800

    change the club to kls

--- take1.js
+++ take1.js
@@ -1,9 +1,2 @@
-function learnMore() {
-    alert("Learn more about the Engineering Club by exploring our About section below!");
-  }
-  
-  document.getElementById("contactForm").addEventListener("submit", function (event) {
-    event.preventDefault();
-    alert("Thank you for your message! We'll get back to you soon.");
-  });
+
   
