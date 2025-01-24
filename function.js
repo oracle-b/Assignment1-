7efd8d42ec5ec503654acb948e29bc862258be75
oracle-b <m.faizrosli11@gmail.com>
2025-01-24 21:43:51 +0800

    18th update

--- function.js
+++ function.js
@@ -1,7 +1,8 @@
-function toggleNavbar() {
-    const nav = document.querySelector('.nav');
-    nav.classList.toggle('active');
-  }
-  
-  const hamburger = document.querySelector('.hamburger');
-  hamburger.addEventListener('click', toggleNavbar);
+function openModal(imageSrc) {
+  document.getElementById("image-modal").style.display = "block";
+  document.getElementById("modal-image").src = imageSrc;
+}
+
+function closeModal() {
+  document.getElementById("image-modal").style.display = "none";
+}
