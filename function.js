cab9852fdfff4c3f6b925de5c32f6e5fe0219480
oracle-b <m.faizrosli11@gmail.com>
2025-01-25 00:51:00 +0800

    20th update adding video

--- function.js
+++ function.js
@@ -6,3 +6,15 @@ function openModal(imageSrc) {
 function closeModal() {
   document.getElementById("image-modal").style.display = "none";
 }
+function toggleFullscreen() {
+  const video = document.querySelector('.video-player');
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.mozRequestFullScreen) { /* Firefox */
+    video.mozRequestFullScreen();
+  } else if (video.webkitRequestFullscreen) { /* Chrome, Safari */
+    video.webkitRequestFullscreen();
+  } else if (video.msRequestFullscreen) { /* IE/Edge */
+    video.msRequestFullscreen();
+  }
+}
